64d39b7e15130f13ba7c3b003b229e6930401778
RubensCarvalhoRocha <rocharubens4@gmail.com>
2025-04-02 20:49:12 -0300

    ajustes

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_restx import Api
 
 from controllers.beneficiario_por_enquadramento_controller import api as beneficiario_ns
 from controllers.populacao_estados_controller import api as populacao_estados_ns
+from controllers.valor_total_beneficio_controller import api as valor_total_ns
+from controllers.uf_total_beneficiarios_controller import api as total_beneficiarios_ns
 
 def create_app():
     app = Flask(__name__)
@@ -17,6 +19,8 @@ def create_app():
 
     api.add_namespace(beneficiario_ns, path="/beneficiarios")
     api.add_namespace(populacao_estados_ns, path="/populacao-estados")
+    api.add_namespace(valor_total_ns, path="/valores-totais")
+    api.add_namespace(total_beneficiarios_ns, path="/totais-uf")
 
     with app.app_context():
         db.create_all()
